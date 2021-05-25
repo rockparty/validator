76f2ce98fa8e5f615fd46deae6b2b7aa1a1c2303
William Fank Thomé <willegp88@gmail.com>
2021-05-25 10:02:52 -0300

    refactor(test): use assertIsValidateErrorMap for expected value

--- src/__tests__/validators/__helpers__/type-validator.helper.ts
+++ src/__tests__/validators/__helpers__/type-validator.helper.ts
@@ -1,3 +1,4 @@
+import { assertIsValidateErrorMap } from '@/asserts'
 import { ValidateError, Validator } from '@/protocols'
 import {
   DataTypes,
@@ -14,7 +15,7 @@ export const typeHasNoErrors = async <T>(
   const valid = pickMockDataToValidate<T>(type, override)
   const { label, value } = valid
   const result = await validator.validate(value, false, label, [label])
-  const expected = result?.get([label]) === undefined
+  const expected = !assertIsValidateErrorMap(result)
   printIfNotExpected(expected, result)
   return expected
 }
@@ -29,7 +30,7 @@ export const typeHasErrors = async <T>(
 
   for (const toValidate of invalids) {
     const { label, value } = toValidate
-    const errors = await validator.validate(value as any, false, label, [label])
+    const errors = await validator.validate(value, false, label, [label])
     if (!errors) continue
     errors.forEach((error) => result.push(...error))
   }
